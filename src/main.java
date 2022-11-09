2984c86f29572fd3aa557c1027b7dce6e21bd643
Nguyễn Thành Huỳnh Lâm <117795761+NgThHuLa@users.noreply.github.com>
2022-11-09 11:57:39 +0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -2,6 +2,6 @@ import java.lang.*;
 
 public class main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World! How's your day?");
     }
 }
