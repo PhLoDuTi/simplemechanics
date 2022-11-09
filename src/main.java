186bc553463e7d4d763d9f24b2e57489e3775fab
Duy Tien <duytien09092002@gmail.com>
2022-11-09 12:06:32 +0700

    New feature

--- src/main.java
+++ src/main.java
@@ -2,6 +2,15 @@ import java.lang.*;
 
 public class main{
     public static void main(String[] args) {
-        System.out.println("Hello World! How's your day?");
+        Scanner sc = new Scanner(System.in); 
+
+        System.out.println("Hello World! How's your day? \n (1 for \"Yes\", 0 for \"No\"): ");
+
+        int a = sc.nextInt();  
+        if (a == 0){
+            System.out.println("Very good! Have a good one!");
+        } else {
+            System.out.println("Oh well, there's always tomorrow.");
+        }
     }
 }
